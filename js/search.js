ffd3c050f099a33d2c83952328d2cebbce0c262b
fberato <flavioberato@gmail.com>
2021-03-08 12:49:21 -0300

    ok!

--- js/search.js
+++ js/search.js
@@ -1,19 +1,19 @@
 // drop down the menu, and swap the icon to the close icon
-$('.menu').click(function(){
+/* $('.menu').click(function(){
   $(this).toggleClass('icon-menu');
   $(this).toggleClass('icon-cross');
   $('nav').toggleClass('down');
   $('nav li a').removeClass('down');
   $('.search').removeClass('down');
   $('.icon-search').removeClass('icon-cross');
-});
+}); */
 
 //Make sure the menu icon behaves corectly when the menu is open
-$('nav li a').click(function(){
+/* $('nav li a').click(function(){
     $('.menu').addClass('icon-menu');
     $('.menu').removeClass('icon-cross');
     $('nav').toggleClass('down');
-});
+}); */
 
 //show and hide the search bar, also make sure if the menu is open to hide the menu, and ensure the menu icon state is correct
 $('.icon-search').click(function(){
